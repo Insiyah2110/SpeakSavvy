88777d46a2829e5e5de4f36cb41f75b4a3c48578
leiah8 <leiah.nay8@gmail.com>
2024-01-13 18:26:36 -0500

    add comments for changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import subprocess
 import random
 import difflib
 
+#CHANGE THESE
 Common_Sentences = [
     "Bonjour Comment ça va", 
     "Ça va bien merci Et toi" ,
@@ -28,7 +29,8 @@ Common_Sentences = [
     "J'aimerais réserver une table pour deux personnes" ,
     "J'adore la cuisine française" ,
     "J'aimerais acheter un billet pour France" , 
-    "Pouvons-nous avoir le menu, s'il vous plaît" 
+    "Pouvons-nous avoir le menu, s'il vous plaît", 
+	"voulez-vous coucher avec moi", "je suis une grenouille", "je n'aime pas les chats", "je m'appelle claude", "bonjour", "s'il te plaît"
     ]
 
 # Initialize the recognizer
@@ -39,7 +41,7 @@ done = False
 # Function to convert text to speech
 def speak_text(command, language="en-us"):
     # Select the voice depending on the language
-    voice = "Samantha" if language == "en-us" else "Thomas"
+    voice = "Samantha" if language == "en-us" else "Thomas"  #CHANGE THOMAS
     # Using the built-in macOS "say" command for text-to-speech
     subprocess.run(["say", "-v", voice, command])
 
@@ -52,7 +54,7 @@ def prompt_and_repeat(sentence):
 
     # The French sentence with the voice Thomas
     print(sentence)
-    speak_text(sentence, "Thomas")
+    speak_text(sentence, "Thomas")  #CHANGE THOMAS
 
     # Use the microphone as source for input
     with sr.Microphone() as source:
@@ -63,7 +65,7 @@ def prompt_and_repeat(sentence):
 
         # Try to recognize the speech in French
         try:
-            spoken_text = r.recognize_google(audio, language="fr-FR")
+            spoken_text = r.recognize_google(audio, language="fr-FR") #CHANGE HERE
             spoken_text = spoken_text.lower()
             print("You said: " + spoken_text)
 
@@ -98,7 +100,6 @@ def prompt_and_repeat(sentence):
 
 # The French sentence you want the user to repeat
 count = 0
-L = ["voulez-vous coucher avec moi", "je suis une grenouille", "je n'aime pas les chats", "je m'appelle claude", "bonjour", "s'il te plaît"]
 size = len(Common_Sentences)
 while(count < 3):
     choice = random.randint(0, size-1)
