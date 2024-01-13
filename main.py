591ec1f0b3a9f574e889ebac93ac07b912a454d0
leiah8 <leiah.nay8@gmail.com>
2024-01-13 16:48:23 -0500

    stuff

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import speech_recognition as sr
 import subprocess
+import random
 
 # Initialize the recognizer
 r = sr.Recognizer()
+
+done = False
 
 # Function to convert text to speech
 def speak_text(command, language="en-us"):
@@ -26,11 +29,14 @@ def prompt_and_repeat(sentence):
         # Adjust for ambient noise and record the audio
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
+        spoken_text = ""
 
         # Try to recognize the speech in French
         try:
             spoken_text = r.recognize_google(audio, language="fr-FR")
             print("You said: " + spoken_text)
+			
+
         except sr.UnknownValueError:
             # If speech is unintelligible
             print("I couldn't understand what you said. Please try again.")
@@ -39,8 +45,30 @@ def prompt_and_repeat(sentence):
         except sr.RequestError:
             # If there's a problem with the speech recognition service
             print("Could not request results; check your internet connection.")
+        test = (str(spoken_text).lower().lstrip()).rstrip()
+        print(";" + test + ";")
+        print(";" + sentence_to_repeat + ";")
+        if (test == sentence_to_repeat.lower()):
+            done = True
+
 
 # The French sentence you want the user to repeat
-sentence_to_repeat = "La pratique rend parfait."
+count = 0
+L = ["voulez vous coucher avec moi", "je suis une grenouille", "je n'aime pas les chats", "je m'appelle claude", "bonjour", "s'il te plaît"]
+size = len(L)
+while(count < 10):
+    choice = random.randint(0, size-1)
+    sentence_to_repeat = L[choice]
+
+    while (not(done)):
+        prompt_and_repeat(sentence_to_repeat)
+        #if(not(done)):
+        #    print("Please Try Again")
+    count += 1
+
+
 
 prompt_and_repeat(sentence_to_repeat)
+
+#please try again until you get it 
+#also loop for different scentences
